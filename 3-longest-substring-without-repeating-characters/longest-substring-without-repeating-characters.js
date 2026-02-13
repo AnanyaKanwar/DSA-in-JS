30344e73d326c595eaa8b79f44fc913084bfaffb
AnanyaKanwar <138487599+AnanyaKanwar@users.noreply.github.com>
2026-02-13 10:08:18 +0530

    Time: 6 ms (67.43%) | Memory: 58.1 MB (60.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -1,25 +1,24 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLongestSubstring = function (s) {
-    let maxlen = 0;
-    let left = 0;
-    let set = new Set();
-    let count = 0;
-    for (let right = 0; right < s.length; right++) {
-        while (set.has(s[right])) {
-            set.delete(s[left]);
-            left++;
+var lengthOfLongestSubstring = function(s) {
+    let temp = new Set();
+    let n = s.length;
+    let i = 0; // Right pointer
+    let j = 0; // Left pointer
+    let maxCount = 0;
+
+    while (i < n) {
+        // If we find a duplicate, shrink the window from the left (j)
+        // until the duplicate character s[i] is removed from the set
+        while (temp.has(s[i])) {
+            temp.delete(s[j]);
+            j++;
         }
-        set.add(s[right]);
-        count = right - left + 1;
-        maxlen = Math.max(count, maxlen);
+
+        // Add the current character and update max
+        temp.add(s[i]);
+        maxCount = Math.max(maxCount, temp.size);
+        
+        i++;
     }
 
-    return maxlen;
-
-
-
-
+    return maxCount;
 };
