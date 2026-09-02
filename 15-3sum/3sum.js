bac88b0fd779cb6bea32c9bf610a322ceebeab48
Ananya Kanwar <138487599+AnanyaKanwar@users.noreply.github.com>
2026-09-02 21:33:44 +0530

    Time: 29 ms (92.16%) | Memory: 71.9 MB (75.63%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,30 +3,39 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let n= nums.length;
+    const result = [];
+    
     nums.sort((a, b) => a - b);
-    let ans= [];
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        
 
-    for(let i=0; i<n-2; i++){
-        //duplicate case
-        if(i > 0 && nums[i] === nums[i-1]) continue;
-        let left= i+1, right= n-1;
+        if (nums[i] > 0) break;
+        
+        let left = i + 1;
+        let right = nums.length - 1;
+        
 
-        while(left < right){
-            let sum= nums[i] + nums[left] + nums[right];
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                
 
-            if(sum === 0) {
-                ans.push([nums[i], nums[left], nums[right]]);
-
-                //duplicate case
-                while(left<right && nums[left] === nums[left+1]) left++;
-                while(left<right && nums[right] === nums[right-1]) right--;
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                
                 left++;
                 right--;
+            } else if (sum < 0) {
+                left++; 
+            } else {
+                right--; 
             }
-            else if (sum < 0) left++;
-            else right--;
         }
     }
-    return ans;
+    
+    return result;
 };
